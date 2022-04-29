5951a674273151b33534b55b0146c4f9d1fba9a3
KhadijaY <102887672+KhadijaY@users.noreply.github.com>
2022-04-28 19:41:55 -0700

    Add files via upload

---
+++ Final Project Proposal .R
@@ -0,0 +1,22 @@
+Topic: Covid Vaccination hesitancy among People of Color 
+Why are you interested in this field/domain?
+  Since we all are majoring in healthcare related majors (Public Health, Medical Anthropology) we were interested in seeing data on vaccination hesitancy rates among people of color. 
+What other examples of data driven project have you found related to this domain (share at least 3)?
+  we looked at three cdc data sets 
+- Rates of Covid-19 Cases or Deaths by age group and vaccination status 
+- Vaccine Hesitancy for Covid 
+- Distribution of Covid-19 Deaths and Population, by juridiction, age, and race, and Hispanic orgin
+What data-driven questions do you hope to answer about this domain (share at least 3)?
+  we want to know how has covid impacted people of color?
+  What are their views on vaccines, like covid-19? 
+  how has the their relationship with the healthcare industry changed pre and post pandemic?
+  Where did you download the data (e.g., a web URL)?
+  We downloaded it from the CDC website.
+How was the data collected or generated? Make sure to explain who collected the data (not necessarily the same people that host the data), and who or what the data is about?
+  It was collected through health departments that linked the cases of immunization to the public health data surrviellence system. The data is based on the positive cases as well as deaths and immunizations of individuals through the dates feb-march 2019.
+How many observations (rows) are in your data?
+  997
+How many features (columns) are in the data?
+  16
+What questions (from above) can be answered using the data in this dataset?
+  Some questions that can be asked when looking at this data is the role that positive cases have with immunization as well as the influence values have on indivuals taking the vaccine. 
